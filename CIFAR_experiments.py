b6fcf07b5928ce9ba9ecdbf6ac9ba835b41fc124
rbarboni <raphael.barboni92@gmail.com>
2022-05-16 17:06:35 +0200

    cifar

--- CIFAR_experiments.py
+++ CIFAR_experiments.py
@@ -43,6 +43,7 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
+
 train_set = datasets.CIFAR10(root='CIFAR',
                               train=True,
                               download=True,
